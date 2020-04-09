3ec9d9c3f54ca6e95aa292684cc4b20f6371dcc3
RamGit15 <63382271+RamGit15@users.noreply.github.com>
2020-04-08 22:14:37 -0500

    Capstone1.py

---
+++ Capstone1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # H  This project is part of Capstone.
+# ## H This will be added to the Github repository I have created
+# 
+# 
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
